20611062599f2be3a83b20f52a28aedd16a2a99e
Ankitha4 <anki.shetty20@gmail.com>
2025-09-26 00:11:15 +0530

    Second Commit

--- BrokenLinks.py
+++ BrokenLinks.py
@@ -1,3 +1,4 @@
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -7,3 +8,18 @@ driver.get("https://example.com/")
 links = driver.find_elements(By.TAG_NAME,"a")
 
 broken_links = []
+
+for link in links:
+    url = link.get_attribute("href")
+    if url == None or url == " ":
+        continue
+    try:
+        response = requests.head(url,timeout= 10)
+        if response.status_code >= 400:
+            print(f"Broken link {url} : {response.status_code}")
+            broken_links.append(url)
+    except requests.exceptions.RequestException as e:
+        print(f" Error with link {url}: {e}")
+        broken_links.append(url)
+
+print(f"Broken links : {len(broken_links)}")
